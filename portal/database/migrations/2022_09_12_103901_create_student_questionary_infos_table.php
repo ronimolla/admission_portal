dc5204af863ecbc5abf5c70c92548f215443bb0d
ronimolla <mollaroni12@gmail.com>
2022-11-30 11:05:37 +0600

    database update

--- portal/database/migrations/2022_09_12_103901_create_student_questionary_infos_table.php
+++ portal/database/migrations/2022_09_12_103901_create_student_questionary_infos_table.php
@@ -17,19 +17,19 @@ return new class extends Migration
             $table->string('student_id');
             $table->string('program_name');
             $table->string('program_batch_id');
-            $table->string('justify_answer')->nullable();
-            $table->string('ambition')->nullable();
-            $table->string('short_bio')->nullable();
-            $table->string('analytics_ans')->nullable();
-            $table->string('narrative_writing_1')->nullable();
-            $table->string('narrative_writing_2')->nullable();
+            $table->longText('justify_answer')->nullable();
+            $table->longText('ambition')->nullable();
+            $table->longText('short_bio')->nullable();
+            $table->longText('analytics_ans')->nullable();
+            $table->longText('narrative_writing_1')->nullable();
+            $table->longText('narrative_writing_2')->nullable();
             $table->string('mcq_1')->nullable(); 
             $table->string('mcq_2')->nullable(); 
             $table->string('mcq_3')->nullable(); 
             $table->string('mcq_4')->nullable(); 
             $table->string('mcq_5')->nullable(); 
-            $table->string('question_1')->nullable(); 
-            $table->string('question_2')->nullable(); 
+            $table->longText('question_1')->nullable(); 
+            $table->longText('question_2')->nullable(); 
             $table->timestamps();
         });
       
