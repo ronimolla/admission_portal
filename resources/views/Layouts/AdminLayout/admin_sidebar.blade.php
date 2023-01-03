66c9c98681d2c41f96784d90ad4868114986e2c5
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2023-01-03 17:13:53 +0600

    sidebar updated

--- resources/views/Layouts/AdminLayout/admin_sidebar.blade.php
+++ resources/views/Layouts/AdminLayout/admin_sidebar.blade.php
@@ -16,81 +16,18 @@
         <div class="" data-simplebar style="height: 100%;">
             <!-- sidebar menu -->
             <ul class="nav sidebar-inner" id="sidebar-menu">
+
+                <li class="{{ request()->is('overview') ? 'active' : '' }}" >
+                    <a class="sidenav-item-link" href="{{url('/over-view')}}">
+                        <i class="mdi mdi-relative-scale"></i>
+                        <span class="nav-text">Overview</span> 
+                    </a>
+                </li>
               
                 <li class="{{ request()->is('dashboard') ? 'active' : '' }}" >
                     <a class="sidenav-item-link" href="{{url('/dashboard')}}">
                         <i class="mdi mdi-view-dashboard-outline"></i>
                         <span class="nav-text">Dashboard</span> 
-                    </a>
-                </li>
-                
-
-                <li class="has-sub ">
-                    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#components"
-                        aria-expanded="false" aria-controls="components">
-                        <i class="mdi mdi-folder-multiple-outline"></i>
-                        <span class="nav-text">Assesment</span> <b class="caret"></b>
-                    </a>
-                    
-                    <ul class="{{ request()->is('assesment/preselection') || request()->is('assesment/writing') || request()->is('assesment/interview') || request()->is('assesment/financialaid') || request()->is('assesment/payment') ? '' : 'collapse' }}  " id="components" data-parent="#sidebar-menu">
-                        <div class="sub-menu">
-                            <li class="has-sub " >
-                                <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#buttons" aria-expanded="false" aria-controls="buttons">
-                                    <span class="nav-text">Program</span> <b class="caret"></b>
-                                </a>
-
-                                <ul class="{{ request()->is('assesment/preselection') || request()->is('assesment/writing')|| request()->is('assesment/interview') || request()->is('assesment/financialaid') || request()->is('assesment/payment')  ? '' : 'collapse' }} " id="buttons">
-                                    <div class="sub-menu">
-                                        <li class="{{ request()->is('assesment/preselection') ? 'active' : '' }}">
-                                             <a href="{{url('/assesment/preselection')}}">Pre Selection</a>
-                                        </li>
-                                        <li class="{{ request()->is('assesment/writing') ? 'active' : '' }}">
-                                            <a href="{{url('/assesment/writing')}}">Writing test</a>
-                                        </li>
-                                        <li class="{{ request()->is('assesment/interview') ? 'active' : '' }}">
-                                            <a href="{{url('/assesment/interview')}}">Interview</a>
-                                        </li>
-                                        <li class="{{ request()->is('assesment/financialaid') ? 'active' : '' }}">
-                                            <a href="{{url('/assesment/financialaid')}}">Waiver Request</a>
-                                        </li>
-                                        <li class="{{ request()->is('assesment/payment') ? 'active' : '' }}">
-                                            <a href="{{url('/assesment/payment')}}">Payment & Registration</a>
-                                        </li>
-                                    </div>
-                                </ul>
-                            </li>
-                            <li class="has-sub ">
-                                <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#buttons1" aria-expanded="false" aria-controls="buttons">
-                                    <span class="nav-text">Event</span> <b class="caret"></b>
-                                </a>
-                                <ul class="collapse " id="buttons1">
-                                    <div class="sub-menu">
-                                        <li class="">
-                                             <a href="button-default.html">Button Default</a>
-                                        </li>
-
-                                        <li class="">
-                                            <a href="button-dropdown.html">Button Dropdown</a>
-                                        </li>
-                                    </div>
-                                </ul>
-                            </li>
-                        </div>
-                    </ul>
-                </li>
-
-                <li class="{{ request()->is('Admins') ? 'active' : '' }}">
-                    <a class="sidenav-item-link" href="{{url('Admins')}}">
-                        <i class="mdi mdi-account-box-outline"></i>
-                        <span class="nav-text">Users</span> 
-                    </a>
-                </li>
-
-
-                <li class="">
-                    <a class="sidenav-item-link" href="{{url('/students/programs_info')}}">
-                        <i class="mdi mdi-book-open"></i>
-                        <span class="nav-text">Student management</span> 
                     </a>
                 </li>
 
@@ -139,6 +76,56 @@
                   </ul>
                 </li>
 
+                
+
+                <li class="has-sub ">
+                    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#components"
+                        aria-expanded="false" aria-controls="components">
+                        <i class="mdi mdi-folder-multiple-outline"></i>
+                        <span class="nav-text">Assesment</span> <b class="caret"></b>
+                    </a>
+                    
+                    <ul class="{{ request()->is('assesment/preselection') || request()->is('assesment/writing') || request()->is('assesment/interview') || request()->is('assesment/financialaid') || request()->is('assesment/payment') ? '' : 'collapse' }}  " id="components" data-parent="#sidebar-menu">
+                        <div class="sub-menu">
+                            <li class="has-sub " >
+                                <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#buttons" aria-expanded="false" aria-controls="buttons">
+                                    <span class="nav-text">Program</span> <b class="caret"></b>
+                                </a>
+
+                                <ul class="{{ request()->is('assesment/preselection') || request()->is('assesment/writing')|| request()->is('assesment/interview') || request()->is('assesment/financialaid') || request()->is('assesment/payment')  ? '' : 'collapse' }} " id="buttons">
+                                    <div class="sub-menu">
+                                        <li class="{{ request()->is('assesment/preselection') ? 'active' : '' }}">
+                                             <a href="{{url('/assesment/preselection')}}">Pre-Selection</a>
+                                        </li>
+                                        <li class="{{ request()->is('assesment/writing') ? 'active' : '' }}">
+                                            <a href="{{url('/assesment/writing')}}">Writing Test</a>
+                                        </li>
+                                        <li class="{{ request()->is('assesment/interview') ? 'active' : '' }}">
+                                            <a href="{{url('/assesment/interview')}}">Interview</a>
+                                        </li>
+                                        <li class="{{ request()->is('assesment/financialaid') ? 'active' : '' }}">
+                                            <a href="{{url('/assesment/financialaid')}}">Waiver Request</a>
+                                        </li>
+                                        <li class="{{ request()->is('assesment/payment') ? 'active' : '' }}">
+                                            <a href="{{url('/assesment/payment')}}">Payment & Registration</a>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </li>
+                        </div>
+                    </ul>
+                </li>
+
+
+
+
+                <li class="">
+                    <a class="sidenav-item-link" href="{{url('/students/programs_info')}}">
+                        <i class="mdi mdi-account-group"></i>
+                        <span class="nav-text">Students DB</span> 
+                    </a>
+                </li>
+
 
                 <li class="has-sub {{ request()->is('old_data/programs') || request()->is('old_data/events') ? 'active' : '' }} ">
                   <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#old"
@@ -165,12 +152,37 @@
                 </li>
 
 
-                <li class="{{ request()->is('admin/setings') ? 'active' : '' }}">
-                    <a class="sidenav-item-link " href="{{url('admin/setings')}}">
-                        <i class="mdi mdi-settings"></i>
-                        <span class="nav-text">Settings</span> 
+                <li class="has-sub {{ request()->is('') || request()->is('') ? 'active' : '' }} ">
+                  <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#old"
+                    aria-expanded="false" aria-controls="old">
+                    <i class="mdi mdi-book-open"></i>
+                    <span class="nav-text">Forms</span> <b class="caret"></b>
+                  </a>
+
+                  <ul class="{{ request()->is('') || request()->is('') ? '' : 'collapse' }} " id="old" data-parent="#sidebar-menu">
+                    <div class="sub-menu">
+                      <li class="{{ request()->is('form_collection') ? 'active' : '' }}">
+                        <a class="sidenav-item-link   " href="{{url('forms')}}">
+                          <span class="nav-text">Programs</span>
+
+                        </a>
+                      </li>
+                      <li class="{{ request()->is('') ? 'active' : '' }}">
+                        <a class="sidenav-item-link" href="">
+                          <span class="nav-text">Events</span>
+                        </a>
+                      </li>
+                    </div>
+                  </ul>
+                </li>
+
+                <li class="{{ request()->is('Admins') ? 'active' : '' }}">
+                    <a class="sidenav-item-link" href="{{url('Admins')}}">
+                        <i class="mdi mdi-account-box-outline"></i>
+                        <span class="nav-text">Users</span> 
                     </a>
                 </li>
+
 
             </ul>
         </div>
--- resources/views/admin_login.blade.php
+++ resources/views/admin_login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Sleek Dashboard - Free Bootstrap 4 Admin Dashboard Template and UI Kit. It is very powerful bootstrap admin dashboard, which allows you to build products like admin panels, content management systems and CRMs etc.">
+    <meta name="description" content="Admission Portal - An automated admission management system">
     <title>Sign In - Admission Portal</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet" />
     <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
